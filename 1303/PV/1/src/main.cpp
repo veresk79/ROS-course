8ad24f4dc40f7e13d7d24e65a5dafa6d05fd230e
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2016-09-26 13:06:53 +0300

    added_cycle

--- 1303/PV/1/src/main.cpp
+++ 1303/PV/1/src/main.cpp
@@ -1,5 +1,4 @@
 #include "ros/ros.h"
-#include "turtlesim/Pose.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Twist.h"
 
@@ -11,15 +10,26 @@ int main(int argc, char** argv) {
 	ros::Publisher publisher = node.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
 	ros::Rate loop_rate(1);
 
-	srand(time(NULL));
+	geometry_msgs::Twist msg;
+	msg.linear.x += 20;
+	msg.linear.y = 0;
+	msg.linear.z = 0;
+	msg.angular.x = 0;
+	msg.angular.y = 0;
+	msg.angular.z = 0;
+	
+	publisher.publish(msg);
+	ros::spinOnce();
+	loop_rate.sleep();
+
 	while(ros::ok()) {
 		geometry_msgs::Twist msg;
-		msg.linear.x = (double) (rand()%10+1)/4.0;
+		msg.linear.x += 1;
 		msg.linear.y = 0;
 		msg.linear.z = 0;
 		msg.angular.x = 0;
 		msg.angular.y = 0;
-		msg.angular.z = (double) (rand()%10-5)/2.0;
+		msg.angular.z += 2;
 		
 		publisher.publish(msg);
 		ros::spinOnce();
