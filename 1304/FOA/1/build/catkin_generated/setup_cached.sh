59545d826693a637879ffa0415df5a486aa7e1bf
tonycool7 <dkominion@yahoo.com>
2016-09-27 01:03:57 +0300

    lab 1

---
+++ 1304/FOA/1/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tonycool/ros/workspace_lab1/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/tonycool/ros/workspace_lab1/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tonycool/ros/workspace_lab1/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tonycool/ros/workspace_lab1/src:$ROS_PACKAGE_PATH"
---
+++ 1304/FOA/1/build/lab1/CMakeFiles/lab1.2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.2.dir/src/lab1_publisher.cpp.o"
+  "/home/tonycool/ros/workspace_lab1/devel/lib/lab1/lab1.2.pdb"
+  "/home/tonycool/ros/workspace_lab1/devel/lib/lab1/lab1.2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
